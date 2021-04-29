36c837545958676e07f2a69ef54570f53fcdc8d0
w8f <haya22aws@gmail.com>
2021-04-29 22:58:51 +0900

    ユーザテーブル作成

--- db/init/init_ddl_sample.sql
+++ db/init/init_ddl_sample.sql
@@ -1,10 +1,11 @@
-CREATE TABLE Sample (
-    id         SERIAL PRIMARY KEY,
-    name       varchar(20),
-    age        int,
+CREATE TABLE mst_user (
+    id          SERIAL PRIMARY KEY,
+    user_id     varchar(30),
+    user_nm     varchar(60),
+    mail        varchar(254),
+    password    varchar(256),
     create_date timestamp,
     update_date timestamp
 );
 
-Insert into sample values(1,'sample花子',24,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
-Insert into sample values(2,'sample太郎',25,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+Insert into mst_user values(nextval('mst_user_id_seq'::regclass),'trial_user', 'お試しユーザ','sample@sample.co.jp','password',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
